5624b0eddaedab3a805b9b7840dc8af00bfc6c4f
Farheen khan <farheenkhancc@gmail.com>
2022-10-01 19:44:58 +0530

    Time: 4 ms (96.97%), Space: 10.6 MB (7.28%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        vector<int> ans;
-        int cnt = 0;
-        for(int i = 0 ; i < nums.size() ; i++) {
-            for (int j = 0 ; j < nums.size() ; j++) {
-                if(nums[i] > nums[j])
-                {
-                     cnt++;
-                }
-            }
-             ans.push_back(cnt);
-             cnt = 0;
+        // vector<int> ans;
+        // int cnt = 0;
+        // for(int i = 0 ; i < nums.size() ; i++) {
+        //     for (int j = 0 ; j < nums.size() ; j++) {
+        //         if(nums[i] > nums[j])
+        //         {
+        //              cnt++;
+        //         }
+        //     }
+        //      ans.push_back(cnt);
+        //      cnt = 0;
+        // }
+        // return ans;
+        
+        
+        map<int, int> mp;
+        int n = nums.size();
+        vector<int> snum = nums;
+        sort(snum.begin() , snum.end());
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            mp[snum[i]] = i;
         }
-        return ans;
-    }
+        
+        for (int i = 0 ; i < n ; i++) {
+            nums[i] = mp[nums[i]];
+        }
+        
+        return nums;
+        
+        
+//         map<int,int> mp;    
+//         int n=nums.size();
+//         vector<int> snum=nums;
+        
+//         sort(snum.begin(),snum.end());
+//         for(int i=n-1;i>=0;i--)
+//             mp[snum[i]]=i;
+        
+//         for(int i=0;i<n;i++)
+//             nums[i]=mp[nums[i]];
+        
+//         return nums;
+    } 
 };
-
-
-// int count =0; vector<int> array;
-//     for(int i=0; i<nums.size(); i++)
-//     {
-//         for(int j=0; j<nums.size(); j++)
-//         {
-//             if(nums[i]>nums[j])
-//             {
-//                 count++;
-//             }
-//         }
-//         array.push_back(count);
-//         count=0;
-//     }
-//     return array;
