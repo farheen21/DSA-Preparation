6ab31d332198648bbe1ee208c9acc9e74ce7a258
Farheen khan <farheenkhancc@gmail.com>
2022-10-01 18:06:58 +0530

    Time: 4 ms (40.57%), Space: 7.3 MB (59.81%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int ans = 0;
-        for(int i = 0 ; i< nums.size() ; i++) {
-            for ( int j = i+1 ; j < nums.size() ; j++) {
-                if (nums[i] == nums[j])
-                    ans++;
-            }
+    // Brute force
+    //     int ans = 0;
+    //     for(int i = 0 ; i< nums.size() ; i++) {
+    //         for ( int j = i+1 ; j < nums.size() ; j++) {
+    //             if (nums[i] == nums[j])
+    //                 ans++;
+    //         }
+    //     }
+    //     return ans;
+    // }
+        
+        unordered_map<int,int> umap; //Initializing a Hash Table
+         
+        for(int i=0;i<nums.size();i++) //Iterating through the vector
+        {
+            ++umap[nums[i]];  //Counting the occurences of a number and storing it in value.
+            
         }
-        return ans;
+        int good_pairs = 0;
+        for(auto i:umap) //Using the formula 
+        {
+            int n = i.second; //i.second implies -- value of hash table
+            good_pairs += ((n)*(n-1))/2;
+            
+        }
+        return good_pairs;
+        
+        
     }
+        
 };
