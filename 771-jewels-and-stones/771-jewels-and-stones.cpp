a5551c8256d4117a2784952306189f0735663625
Farheen khan <farheenkhancc@gmail.com>
2022-10-01 18:32:38 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (7.11%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {  
+        // int ans = 0;
+        // sort(jewels.begin() , jewels.end());
+        // for(int i = 0 ; i < stones.length() ; i++) {
+        //     if(binary_search(jewels.begin() , jewels.end() , stones[i]))
+        //      ans++;
+        // }
+        //     return ans;
+        // ---------------
+        set<char> s;
         int ans = 0;
-        sort(jewels.begin() , jewels.end());
-        for(int i = 0 ; i < stones.length() ; i++) {
-            if(binary_search(jewels.begin() , jewels.end() , stones[i]))
-             ans++;
+        for(char i : jewels){
+            s.insert(i);
         }
-            return ans;
+        for(char i : stones){
+            if(s.find(i)!=s.end()){ ans ++;}
+        }
+        return ans;
     }
 };
