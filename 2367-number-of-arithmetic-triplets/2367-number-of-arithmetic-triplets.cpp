3be291d2bbf0e0714b083a9df507b9751bdf8e73
Farheen khan <farheenkhancc@gmail.com>
2022-10-02 11:20:09 +0530

    Time: 11 ms (56.43%), Space: 8.7 MB (34.48%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,15 +1,43 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-      int ans = 0;
-        for(int i = 0 ; i < nums.size() ; i++) {
-            for(int j = i+1 ; j < nums.size() ; j++) {
-               for(int k = j + 1 ; k < nums.size() ; k++) {
-                   if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff)
-                       ans++;
-               }
-            }
+      // int ans = 0;
+      //   for(int i = 0 ; i < nums.size() ; i++) {
+      //       for(int j = i+1 ; j < nums.size() ; j++) {
+      //          for(int k = j + 1 ; k < nums.size() ; k++) {
+      //              if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff)
+      //                  ans++;
+      //          }
+      //       }
+      //   }
+      //   return ans;
+        
+//         int arr[201] = {0};
+//         int ans = 0 ;
+//         for (auto it:nums) {
+//             arr[it]++;
+//         }
+       
+//         for(auto it:nums) {
+//             if( it-diff >= 0 && it+diff <= 200 && arr[it-diff] && arr[it+diff]) {
+//                 ans++;
+//             }
+//         }
+//         return ans;
+        
+        
+        int count = 0;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            auto idx1 = find(nums.begin(), nums.end(), nums[i]+diff);
+            auto idx2 = find(nums.begin(), nums.end(), nums[i]+2*diff);
+            
+            if(idx1 != nums.end() && idx2 != nums.end())
+                count++;
+            else
+                continue;
         }
-        return ans;
+        return count;
     }
 };
