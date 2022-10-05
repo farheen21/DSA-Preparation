509d89575fd79a90dddb30ae248ef06dd6a6628c
Farheen khan <farheenkhancc@gmail.com>
2022-10-05 09:54:32 +0530

    Time: 12 ms (96.00%), Space: 13.3 MB (54.59%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        // Edge Case Checking 
+     if (matrix.empty() || matrix[0].empty()) return;
         
-      set<int>rows,cols;
-        
-        for(int i = 0;i<matrix.size();i++){
-            for(int j = 0;j<matrix[i].size();j++){
-                if(matrix[i][j]==0){
-                    rows.insert(i);
-                    cols.insert(j);
-                }
-            }
-        }
-        
-        
-        //if we encounter either that row or column number set it to zero
-        for(int i = 0;i<matrix.size();i++)
-            for(int j = 0;j<matrix[i].size();j++)
-                if(rows.find(i)!=rows.end() or cols.find(j)!=cols.end())
-                       matrix[i][j] = 0;
-        
-                
-       
-    }
+	const int m = matrix.size(), n = matrix[0].size();
+	bool col_zero = false, row_zero = false;
+	
+	for (int i = 0; i < m; i++) {
+		if (matrix[i][0] == 0) col_zero = true;
+	}
+	
+	for (int i = 0; i < n; i++) {
+		if (matrix[0][i] == 0) row_zero = true;
+	}
+	
+	for (int i = 1; i < m; i++) {
+		for (int j = 1; j < n; j++) {
+			if (matrix[i][j] == 0) {
+				matrix[0][j] = 0;
+				matrix[i][0] = 0;
+			}
+		}
+	}
+	
+	for (int i = 1; i < m; i++) {
+		for (int j = 1; j < n; j++) {
+			if (matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
+		}
+	}
+	
+	if (row_zero == true) {
+		for (int i = 0; i < n; i++) {
+			matrix[0][i] = 0;
+		}
+	}
+	if (col_zero == true) {
+		for (int i = 0; i < m; i++) {
+			matrix[i][0] = 0;
+		}
+	}
+}
 };
+    
